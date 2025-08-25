f52b077914f6a344c6bb0eb838da6d7857804e0c
kkmecha <167722055+kkmecha@users.noreply.github.com>
2025-08-25 21:09:38 +0900

    Add files via upload

--- IncEnc_board.cpp
+++ IncEnc_board.cpp
@@ -1,42 +1,49 @@
-#include "mbed.h"
 #include "IncEnc_board.h"
-#include <vector>
-#include <map>
 
-IncEnc_board::IncEnc_board(CAN &can, int num, uint32_t start_id = 1) : _can(can){
-    // 指定された個数と開始IDから、管理対象のIDリストを自動生成する
-    for (int i = 0; i < _num; ++i) {
+#define CAN_RX_FLAG (1UL << 0)
+
+IncEnc_board::IncEnc_board(CAN &can, int num, uint32_t start_id)
+ : _can(can) {
+    for (int i = 0; i < num; ++i) {
         uint32_t current_id = start_id + i;
         _node_ids.push_back(current_id);
-        // マップを初期化
         _angle_map[0x400 + current_id] = 0;
     }
     _can.attach(callback(this, &IncEnc_board::_rx_isr));
- }
+    _read_thread.start(callback(this, &IncEnc_board::_read_thread_func));
+}
 
-void IncEnc_board::read_all(int64_t* angle) {
+void IncEnc_board::read_all(int64_t* angles) {
     for (size_t i = 0; i < _node_ids.size(); ++i) {
         uint32_t can_id = 0x400 + _node_ids[i];
-        angle[i] = _angle_map[can_id];
+        angles[i] = _angle_map[can_id];
+    }
+}
+
+void IncEnc_board::_rx_isr() {
+    _event_flags.set(CAN_RX_FLAG);
+}
+
+void IncEnc_board::_read_thread_func() {
+    while (true) {
+        _event_flags.wait_any(CAN_RX_FLAG);
+
+        CANMessage msg;
+        while (_can.read(msg)) {
+            if (_angle_map.count(msg.id)) {
+                int64_t received_angle = 0;
+                for (int i = 0; i < 8; ++i) {
+                    received_angle |= ((int64_t)msg.data[i] << (8 * (7 - i)));
+                }
+                _angle_map[msg.id] = received_angle;
+            }
+        }
     }
 }
 
 bool IncEnc_board::reset_node(uint32_t node_id) {
     uint32_t can_id = 0x400 + node_id;
     const uint8_t reset_command[] = {0xff};
-    CANMessage msg(can_id, reset_command);
+    CANMessage msg(can_id, reset_command, 1);
     return _can.write(msg);
-}
-
-void IncEnc_board::_rx_isr() {
-    CANMessage msg;
-    if (_can.read(msg)) {
-        if (_angle_map.count(msg.id)) {
-            int64_t received_angle = 0;
-            for (int i = 0; i < 8; ++i) {
-                received_angle |= ((int64_t)msg.data[i] << (8 * (7 - i)));
-            }
-            _angle_map[msg.id] = received_angle;
-        }
-    }
 }
--- IncEnc_board.h
+++ IncEnc_board.h
@@ -1,22 +1,23 @@
-#ifndef __INCENC_BOARD_H__
-#define __INCENC_BOARD_H__
+#pragma once
 
 #include "mbed.h"
 #include <vector>
 #include <map>
 
-class IncEnc_board{
-    public:
-        IncEnc_board(CAN &can, int num, uint32_t start_id = 1);
-        void read_all(int64_t* angles);
-        bool reset_node(uint32_t node_id);
+class IncEnc_board {
+public:
+    IncEnc_board(CAN &can, int num, uint32_t start_id = 1);
+    void read_all(int64_t* angles);
+    bool reset_node(uint32_t node_id);
 
-    private:
-        void _rx_isr();
+private:
+    void _rx_isr(); 
+    void _read_thread_func();
 
-        CAN& _can;
-        std::vector<uint32_t> _node_ids;
-        std::map<uint32_t, int64_t> _angle_map;
+    CAN& _can;
+    std::vector<uint32_t> _node_ids;
+    std::map<uint32_t, int64_t> _angle_map;
+
+    Thread _read_thread; 
+    EventFlags _event_flags; 
 };
-
-#endif // __INCENC_BOARD_H__
