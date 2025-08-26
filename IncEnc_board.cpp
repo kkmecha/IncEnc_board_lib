badd20f7232edf849b03cff25d2fadfa9090e4d3
kkmecha <167722055+kkmecha@users.noreply.github.com>
2025-08-26 10:04:06 +0900

    Add files via upload

--- IncEnc_board.cpp
+++ IncEnc_board.cpp
@@ -2,15 +2,26 @@
 
 IncEnc_board::IncEnc_board(int all_node_num) : _all_node_num(all_node_num){}
 
+void IncEnc_board::encoder_calib_node(CAN &can, CANMessage &msg, int node){
+    msg.id = 0x400 + node;
+    msg.data[0] = 0xff;
+    can.write(msg);
+}
+
+void IncEnc_board::encoder_calib_all(CAN &can, CANMessage &msg){
+    for(int id = 0; id < _all_node_num; id++){
+        this->encoder_calib_node(can, msg, id);
+    }
+}
+
 void IncEnc_board::conv_data_node(CANMessage &msg, int64_t *angle, uint8_t node){
     if(msg.id == 0x400 + node){
-        for(int i = 0; i < _all_node_num; i++){
-            *angle |= ((int64_t)msg.data[i] >> (8 * (7 - i)));
+        for(int id = 0; id < _all_node_num; id++){
+            *angle |= ((int64_t)msg.data[id] >> (8 * (7 - id)));
         }
     }
 }
 
 void IncEnc_board::conv_data_all(CANMessage &msg, int64_t *angles){
     for(int node = 0; node < _all_node_num; node++) this->conv_data_node(msg, angles, node);
-
 }
--- IncEnc_board.h
+++ IncEnc_board.h
@@ -5,6 +5,8 @@
 class IncEnc_board {
 public:
     IncEnc_board(int all_node_num);
+    void encoder_calib_node(CAN &can, CANMessage &msg, int node);
+    void encoder_calib_all(CAN &can, CANMessage &msg);
     void conv_data_node(CANMessage &msg, int64_t* angle, uint8_t node);
     void conv_data_all(CANMessage &msg, int64_t* angles);
 
